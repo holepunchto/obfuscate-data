46928583e5f71c11168dd90959517911bd3344f7
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2026-07-21 22:03:46 +0200

    fix v0 missing hash (#9)

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ const sodium = require('sodium-universal')
 
 const Payload = {
   preencode(state, m) {
+    c.uint.preencode(state, 0) // sentinel byte
+    c.uint.preencode(state, m.version)
     c.buffer.preencode(state, m.data)
     c.fixed(16).preencode(state, m.salt)
   },
   encode(state, m) {
+    c.uint.encode(state, 0) // sentinel byte
+    c.uint.encode(state, m.version)
     c.buffer.encode(state, m.data)
     c.fixed(16).encode(state, m.salt)
   },
   decode(state, m) {
+    const isLegacy = state.buffer[state.start] !== 0 || state.buffer.length === 17
+
+    if (!isLegacy) state.start++
+
     return {
+      version: isLegacy ? 0 : c.uint.decode(state),
       data: c.buffer.decode(state),
       salt: c.fixed(16).decode(state)
     }
@@ -38,11 +47,12 @@ module.exports = class Obfuscator {
 
     xor(mask, mask, data)
 
-    return c.encode(Payload, { data: mask, salt })
+    return c.encode(Payload, { version: 1, data: mask, salt })
   }
 
   static deobfuscate(payload, key) {
-    const { data, salt } = c.decode(Payload, payload)
+    const { version, data, salt } = c.decode(Payload, payload)
+    if (version === 0) return data // v0 bugged: plaintext
 
     const mask = hash(salt, key, data.byteLength)
 
@@ -55,6 +65,8 @@ module.exports = class Obfuscator {
 function hash(salt, key, length) {
   const hashLength = Math.max(length, sodium.crypto_generichash_BYTES_MIN)
   const buffer = b4a.alloc(hashLength)
+
+  sodium.crypto_generichash(buffer, salt, key)
 
   return hashLength === length ? buffer : buffer.subarray(0, length)
 }
